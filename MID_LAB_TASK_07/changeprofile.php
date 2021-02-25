ae281bd935605dc7993ff422c01398b21d2bcc74
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-02-25 11:47:09 +0600

    change_profile_picture

---
+++ MID_LAB_TASK_07/changeprofile.php
@@ -0,0 +1,42 @@
+ 
+<head>
+     
+    <title>  Change profile form</title>
+</head>
+<body>
+    <fieldset>
+        <table width='90%' >
+            
+                 
+        </table>
+    </fieldset>
+ 
+    <fieldset>
+        <fieldset>
+            <legend>
+                <b> Change  profile </b>
+            </legend>
+            <form action='./dashboard.php' method='POST' >
+                <table align=""> 
+                    <tr>
+                        <td>
+						<b>Edit Image </b>
+                          <label for="img">Select image:</label>
+  <input type="file" id="img" name="img" accept="image/*">
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                         
+                        </td>
+                    </tr>
+					
+					 
+					
+					</table>
+            </form>
+        </fieldset>
+    </fieldset>
+    
+</body>
+</html>
