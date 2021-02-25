8515f44247a5e20b31f551c8a67306d527540a2d
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-02-25 10:18:03 +0600

    Forget_password_form

---
+++ MID_LAB_TASK_07/forget.php
@@ -0,0 +1,61 @@
+<html>
+<head> 
+    <title>forget password</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src='./homepage/userlogo.png' height='60px'/>
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./home.php'>Home</a> |
+                        <a href='./login.php'>Log In</a> |
+                        <a href='./reg.php'>Registration</a> 
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <legend>
+		<h2>
+            <b>Forget Password</b>
+			</h2>
+        </legend>
+        <form action='#' method='POST'>
+            <table> 
+                <tr>
+                    <td>
+                        Enter a valid Email:
+                    </td>
+                    <td>
+                        <input type="email" name='email'>
+                    </td>
+                </tr>
+				<br>
+				 <tr>
+                    <td>
+                        Enter a valid  phone number:
+                    </td>
+                    <td>
+                        <input type="number" name='number'>
+                    </td>
+                </tr>
+				
+				
+                <tr>
+                    
+                </tr>
+                <tr>
+                    <td>
+                        <input type='submit' value='Submit'>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </fieldset>
+</body>
+</html>
