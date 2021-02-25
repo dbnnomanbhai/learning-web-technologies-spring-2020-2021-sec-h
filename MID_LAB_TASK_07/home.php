7857b1ba29caa920dd947d9c1e6c0e0d170ee7a2
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-02-25 09:47:59 +0600

    Homepage

---
+++ MID_LAB_TASK_07/home.php
@@ -0,0 +1,44 @@
+ <html>
+<head>
+     
+    <title>Home Page</title>
+</head>
+<body>
+    <fieldset>
+        <table width='100%' >
+            <tr>
+            <td>
+                    <img src='./homepage/customerlogo.png'   height='60px'/>
+                </td> 
+                <td align='right'>
+                    <nav>
+                        <a href='./home.php'>Home</a> |
+                        <a href='./login.php'>Log In</a> |
+                        <a href='./registration.php'>Registration</a>
+                    </nav>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <br> <br><br>
+        <label>
+            <h1>
+			<b>
+                Welcome to X Company
+				</b>
+            </h1>
+        </label>
+        <br><br><br><br>
+    </fieldset>
+    <fieldset>
+        <center>
+            <label>
+			<b>
+                Copyright © 2017
+				</b>
+            </label>
+        </center>
+    </fieldset>
+</body>
+</html>
