849f780de755398604d7595552a8a990ebe1b2d1
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-03 00:38:41 +0600

    home

---
+++ MID_LAB_TASK_08_JASON/view/home.php
@@ -0,0 +1,21 @@
+<?php
+	$title= "Home Page";
+	include('header.php');
+?>
+
+	<div id="page_title">
+		<h1>Welcome home, <?php echo $_SESSION['current_user']['username'];?></h1>
+	</div>
+
+	<div id='nav_bar'>
+		<a href="create.php"> Create New User</a> |
+		<a href="user_list.php"> View User List</a> |
+		<a href="../controller/logout.php"> logout</a>	
+	</div>
+
+	<div id="main_content">
+		
+	</div>
+
+
+<?php include('footer.php'); ?>
