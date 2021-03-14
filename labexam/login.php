fec9bcbec695bd200faa06f3f8ae5197b58d434b
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-14 13:13:49 +0600

    3r commit

--- labexam/login.php
+++ labexam/login.php
@@ -14,20 +14,27 @@
             LOGIN
         </legend>
 
- <form action="checklogin.php" method="post" style="border: 2px dotted blue; text-align:center; width: 400px;">
-	<p>
-	
-		Username: <input name="username" type="text" value="<?php if(isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>">
-	</p>
-	<p>Password: <input name="password" type="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
-	</p>
-	<p><input type="checkbox" name="remember" /> Remember me  
-	</p>
-	<p><input type="submit" value="Login"></span> <br><br><a href='./recover.php'> Forget Password?</a></p>  
-     	
+ <form action="checklogin.php" method="post">
+	<center>
+<form>
+	<table border="0" cellspacing="0" cellpadding="0">
+		<tr>
+			<td>
+				<fieldset>
+					<legend><h3>LOGIN</h3></legend>
+					User Id<br/>
+					<input type="text"><br/>                               
+					Password<br/>
+					<input type="password">
+					<br /><hr/>
+					<input type="button" value="Login">
+					<a href="registration.html">Register</a>
+				</fieldset>
+			</td>
+		</tr>                                
+	</table>
 </form>
-<nav>
-<a href='./registration.php'> Registration</a>
+</center>
 </nav>
 </fieldset>
 </body>
