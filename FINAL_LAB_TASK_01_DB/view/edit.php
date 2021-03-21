bfac04b224b75c3b906c6805d9bd2e85777b5901
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-21 12:30:50 +0600

    update database

--- FINAL_LAB_TASK_01_DB/view/edit.php
+++ FINAL_LAB_TASK_01_DB/view/edit.php
@@ -1,48 +1,65 @@
 <?php
-	$title= "Edit Page";
+	 
 	include('header.php');
-
-	//echo $_GET['id'];
-	//echo $_GET['email'];
+	require_once('../model/db.php');
 ?>
 
+<?php
 
-	<div id="page_title">
-		<h1>Edit Page</h1>
-	</div>
+	// retrive data from database
+	$id = $_GET['id'];
+	$connection = getConnection();
+	$sql = "select * from registration where id='{$id}'";
+	$result = mysqli_query($connection,$sql);
+	$row = mysqli_fetch_assoc($result);
+?>
 
-	<div id='nav_bar'>
-		<a href="user_list.php"> Back</a> |
-		<a href="../controller/logout.php"> logout</a>	
-	</div>
+<div id="page_title">
+	<h1>Edit Page</h1>
+</div>
+ 
 
-	<div id="main_content">
-		<form method="post" action="">
-			<fieldset>
-				<legend>Create New</legend>
-				<table>
-					<tr>
-						<td>Username</td>
-						<td><input type="text" name="username" value="alamin"> </td>
-					</tr>
-					<tr>
-						<td>Password</td>
-						<td><input type="password" name="password" value="123"> </td>
-					</tr>
-				
-					<tr>
-						<td>Email</td>
-						<td><input type="email" name="email" value="alamin@aiub.edu"> </td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>
-							<input type="submit" name="signup" value="Update">
-						</td>
-					</tr>
-				</table>
-			</fieldset>
-		</form>
-	</div>
+<div id="main_content">
+	<form method="post" action="">
+		<fieldset>
+			<legend>Create New</legend>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="username" value="<?php echo $row['username'] ?>"> </td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password" value="<?php echo $row['password'] ?> "> </td>
+				</tr>
+			
+				<tr>
+					<td>Email</td>
+					<td><input type="email" name="email" value="<?php echo $row['email'] ?> "> </td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<input type="submit" name="update" value="Update">
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</div>
 
-	<?php include('footer.php'); ?>
+<?php 
+	include('footer.php');
+
+	if(isset($_POST['update']))
+	{
+		 
+	 	$sql = "update registration set username='{$_POST['username']}' where id='{$id}'";
+	 	$result = mysqli_query($connection,$sql);
+	 	$sql = "update registration set password='{$_POST['password']}' where id='{$id}'";
+	 	$result = mysqli_query($connection,$sql);
+	 	$sql = "update registration set email='{$_POST['email']}' where id='{$id}'";
+	 	$result = mysqli_query($connection,$sql);
+	 	echo " update";
+	}
+?>
