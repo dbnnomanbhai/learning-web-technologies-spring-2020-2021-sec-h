83bb8cfe78b458ebcf36f96e84f216089de392ff
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-21 14:22:09 +0600

    index

---
+++ FINAL_LAB_TASK_02_DB/index.php
@@ -0,0 +1,15 @@
+ 
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<center>
+		<a href="view/addProduct.php"> Add Product</a>
+		<br>
+		<a href="view/displayProduct.php"> Display Product </a>
+		<br>
+		<a href="searchProduct.php">Search Product</a>
+	</center>
+</body>
+</html>
