0571411f3d824e7320adada6c3086a4d501d16ee
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-23 13:59:40 +0600

    admin checking

---
+++ FINAL_LAB_TASK_03_PHP/controller/adminPasswordCheck.php
@@ -0,0 +1,43 @@
+<?php
+	 
+	require_once('../model/userModel.php');
+	require_once('../model/db.php');
+	if(isset($_POST['change']))
+	{
+		
+		if($_POST['confirmPass'] == "" || $_POST['newPass'] == "" || $_POST['rPass'] == "")
+		{
+			echo "null submission...";
+		}
+		elseif($_POST['newPass'] != $_POST['rPass'])
+		{
+			echo "  password mismatched!";
+		}
+		else
+		{
+			session_start();
+			$currUser = getUserById($_SESSION['currUserId']);
+			$id = $currUser['id'];
+			$password = $currUser['password'];
+			$newPass = $_POST['newPass'];
+			if($_POST['confirmPass'] != $password)
+			{
+				echo " invalid";
+			}
+			else
+			{
+				$check = updateUserPassword($id, $newPass);
+				if($check)
+				{
+					echo "Password changed!";
+				}
+				else
+				{
+					echo "Error occured!";
+				}
+			}
+		}
+
+	}
+
+?>
