7a6731447c19f810059c2089c7082195aeebdc95
dbnnomanbhai <18-38293-2@student.aiub.edu>
2021-03-23 13:42:43 +0600

    registration checking

---
+++ FINAL_LAB_TASK_03_PHP/controller/regCheck.php
@@ -0,0 +1,42 @@
+<?php  
+	
+	require_once('../model/db.php');
+	require_once('../model/userModel.php');
+
+	if(isset($_POST['register']))
+	{
+		$id = $_POST['id'];
+		$password = $_POST['password'];
+		$cPassword = $_POST['confirmPassword'];
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$type = $_POST['type'];
+
+		if($id == "" || $password == "" || $confirmPassword == "" || $name == "" || $email == "")
+		{
+			echo " there has no value inserted";
+		}else
+		{
+
+			if($password == $confirmPassword)
+			{
+				$userDetails = array('id' => $id, 'password' => $password, 'name' => $name, 'email' => $email, 'type' => $type);
+				$check = insertUser($userDetails);
+				if($check)
+				{
+					echo "User added!";
+					header('location: ../view/login.php');
+				}
+				else
+				{
+					echo "Error!";
+				}
+			}
+			else
+			{
+				echo "password & confirm password not same or mismatch";
+			}
+		}
+
+	}
+?>
